5ddfec365bb3039c6148733dfacb63d7076cfaa4
liuzhengyang <liuzhengyang@meituan.com>
2016-09-08 01:46:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## This is [beanstalkd](https://github.com/kr/beanstalkd) client for Go.
+Now it's a quite simple implementation. I'll keep improving and refactoring it.
+
+## Examples
+
+```
+ddr := "localhost:11300"
+	conn, e := net.Dial("tcp", addr)
+	defer conn.Close()
+	if e != nil {
+		panic("connect error")
+	}
+	newConn, _ := NewConn(conn, addr)
+	go Put(newConn, "hello", "test2", 3)
+	Listen(newConn, "test2")
+```
---
+++ src/beanstalk-go/client/Put.go
@@ -0,0 +1,25 @@
+package client
+
+import (
+	"fmt"
+)
+
+type Put struct {
+	pri   int
+	delay int
+	ttr   int
+	//bytes int32
+	body  []byte
+}
+
+func NewPut(pri int, delay int, ttr int, body []byte) *Put {
+	return &Put{pri:pri, delay:delay, ttr:ttr, body:body}
+}
+
+func (this *Put) GetBytes() []byte {
+	str := fmt.Sprintf("put %d %d %d %d\r\n", this.pri, this.delay, this.ttr, len(this.body))
+	res := []byte(str)
+	res2 := append(res, this.body...)
+	res3 := append(res2, []byte("\r\n")...)
+	return res3
+}
