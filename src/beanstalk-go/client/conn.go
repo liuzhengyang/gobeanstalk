68aef921f0692d9f46b0881542f8b2627e452412
liuzhengyang <liuzhengyang@meituan.com>
2016-09-08 21:48:13 +0800

    Extract some method to new file

---
+++ src/beanstalk-go/client/conn.go
@@ -0,0 +1,110 @@
+package client
+
+import (
+	"strconv"
+	"fmt"
+	"net"
+	"bufio"
+	"strings"
+)
+
+type Conn struct {
+	conn      net.Conn
+	addr      string
+	bufReader *bufio.Reader
+	bufWriter *bufio.Writer
+}
+
+func NewConnection(hostAndIp string) *Conn {
+	conn, _ := net.Dial("tcp", hostAndIp)
+	//defer conn.Close()
+	c := new(Conn)
+	c.conn = conn
+	c.addr = hostAndIp
+	c.bufReader = bufio.NewReader(conn)
+	c.bufWriter = bufio.NewWriter(conn)
+
+	return c
+}
+
+// haven't deal with fail conditions
+func sendAndGetOneLine(conn *Conn, command string) string {
+	conn.bufWriter.Write([]byte(command))
+	conn.bufWriter.Flush()
+	line, _, _ := conn.bufReader.ReadLine()
+	res := string(line)
+	fmt.Printf("res : %s\n", res)
+	return res
+}
+
+func (this *Conn) PutWithTube(body string, tube string, delay int) (int, error) {
+	this.Use(tube)
+	return this.Put(body, delay)
+}
+
+func NewConn(conn net.Conn, addr string) (*Conn, error) {
+	c := new(Conn)
+	c.conn = conn
+	c.addr = addr
+	c.bufReader = bufio.NewReader(conn)
+	c.bufWriter = bufio.NewWriter(conn)
+	return c, nil
+}
+
+func (this *Conn) Use(tube string) {
+	command := fmt.Sprintf("use %s\r\n", tube)
+	sendAndGetOneLine(this, command)
+}
+
+func (this *Conn) Watch(tube string) {
+	command := fmt.Sprintf("watch %s\r\n", tube)
+	sendAndGetOneLine(this, command)
+}
+
+func (this *Conn) Put(body string, delay int) (int, error) {
+	command := NewPut(1, delay, 100, []byte(body))
+	this.bufWriter.Write(command.GetBytes())
+	this.bufWriter.Flush()
+	line, _, _ := this.bufReader.ReadLine()
+	fmt.Printf("Put answer %s\n", line)
+	token := strings.Split(string(line), " ")
+	fmt.Println(string(line))
+	jobId, _ := strconv.Atoi(token[1])
+	return jobId, nil
+}
+
+func Reserve(conn *Conn) (int, string) {
+	command := []byte("reserve\r\n")
+	conn.bufWriter.Write(command)
+	conn.bufWriter.Flush()
+	line, _, _ := conn.bufReader.ReadLine()
+	dataline, _, _ := conn.bufReader.ReadLine()
+	tokens := strings.Split(string(line), " ")
+	idstr := tokens[1]
+	id, _ := strconv.Atoi(idstr)
+	fmt.Printf("Reserve %s\n", string(line))
+	fmt.Printf("Reserve %s\n", string(dataline))
+	return id, string(dataline)
+}
+
+func (this *Conn) deleteMessage(id int) {
+	commandStr := fmt.Sprintf("delete %d\r\n", id)
+	command := []byte(commandStr)
+	this.bufWriter.Write(command)
+	this.bufWriter.Flush()
+	line, _, _ := this.bufReader.ReadLine()
+	fmt.Printf("delete %s\n", string(line))
+}
+
+func (this *Conn) Listen(tube string) {
+	listenConnection, _ := net.Dial("tcp", this.addr)
+	newConn, _ := NewConn(listenConnection, this.addr)
+	newConn.Use(tube)
+	newConn.Watch(tube)
+	for {
+		id, data := Reserve(newConn)
+		fmt.Printf("Receive %s\n", data)
+		newConn.deleteMessage(id)
+	}
+}
+
--- src/beanstalk-go/main.go
+++ src/beanstalk-go/main.go
@@ -3,100 +3,18 @@ package main
 import (
 	"fmt"
 	"beanstalk-go/client"
-	"net"
-	"bufio"
-	"strings"
-	"strconv"
 )
-
-type Conn struct {
-	conn      net.Conn
-	addr      string
-	bufReader *bufio.Reader
-	bufWriter *bufio.Writer
-}
-
-func NewConn(conn net.Conn, addr string) (*Conn, error) {
-	c := new(Conn)
-	c.conn = conn
-	c.addr = addr
-	c.bufReader = bufio.NewReader(conn)
-	c.bufWriter = bufio.NewWriter(conn)
-	return c, nil
-}
-
-func (this *Conn) Use(tube string) {
-	command := fmt.Sprintf("use %s\r\n", tube)
-	this.bufWriter.Write([]byte(command))
-	this.bufWriter.Flush()
-	line, _, _ := this.bufReader.ReadLine()
-	fmt.Println(string(line))
-}
-
-func (this *Conn) Watch(tube string) {
-	command := fmt.Sprintf("watch %s\r\n", tube)
-	this.bufWriter.Write([]byte(command))
-	this.bufWriter.Flush()
-	line, _, _ := this.bufReader.ReadLine()
-	fmt.Println(string(line))
-}
-
-func (this *Conn) Put(body string, delay int) {
-	command := client.NewPut(1, delay, 100, []byte(body))
-	this.bufWriter.Write(command.GetBytes())
-	this.bufWriter.Flush()
-	line, _, _ := this.bufReader.ReadLine()
-	fmt.Println(string(line))
-}
-
-func Put(conn *Conn, body string, tube string, delay int) {
-	conn.Use(tube)
-	conn.Put(body, delay)
-}
-
-func Reserve(conn *Conn) (int, string) {
-	command := []byte("reserve\r\n")
-	conn.bufWriter.Write(command)
-	conn.bufWriter.Flush()
-	line, _, _ := conn.bufReader.ReadLine()
-	dataline, _, _ := conn.bufReader.ReadLine()
-	tokens := strings.Split(string(line), " ")
-	idstr := tokens[1]
-	id, _ := strconv.Atoi(idstr)
-	fmt.Printf("Reserve %s\n", string(line))
-	fmt.Printf("Reserve %s\n", string(dataline))
-	return id, string(dataline)
-}
-
-func (this *Conn) deleteMessage(id int) {
-	commandStr := fmt.Sprintf("delete %d\r\n", id)
-	command := []byte(commandStr)
-	this.bufWriter.Write(command)
-	this.bufWriter.Flush()
-	line, _, _ := this.bufReader.ReadLine()
-	fmt.Printf("delete %s\n", string(line))
-}
-
-func Listen(conn *Conn, tube string) {
-	listenConnection, _ := net.Dial("tcp", conn.addr)
-	newConn, _ := NewConn(listenConnection, conn.addr)
-	newConn.Use(tube)
-	newConn.Watch(tube)
-	for {
-		id, data := Reserve(newConn)
-		fmt.Printf("Receive %s\n", data)
-		newConn.deleteMessage(id)
-	}
-}
 
 func main() {
 	addr := "localhost:11300"
-	conn, e := net.Dial("tcp", addr)
-	defer conn.Close()
-	if e != nil {
-		panic("connect error")
+	newConn := client.NewConnection(addr)
+	channel := make(chan int)
+	putFunc := func() {
+		id, _ := newConn.PutWithTube("hello", "test2", 3)
+		channel <- id
 	}
-	newConn, _ := NewConn(conn, addr)
-	go Put(newConn, "hello", "test2", 3)
-	Listen(newConn, "test2")
+	go putFunc()
+	id := <-channel
+	fmt.Printf("Receive from channel message of another goroutine %d", id)
+	newConn.Listen("test2")
 }
